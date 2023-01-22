79eeec01174445e2e8a1dd0ea3478a831488bc48
Green Full Stack <greentree619@outlook.com>
2023-01-23 09:29:00 +1100

    Add FluentValidation In angular frontend

--- EShopping.Frontend/src/app/add-order/add-order.component.ts
+++ EShopping.Frontend/src/app/add-order/add-order.component.ts
@@ -3,14 +3,18 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HttpProviderService } from '../Service/http-provider.service';
+import { Validator } from 'fluentvalidation-ts';
 
 @Component({
   selector: 'app-add-order',
   templateUrl: './add-order.component.html',
   styleUrls: ['./add-order.component.scss']
 })
+
 export class AddOrderComponent implements OnInit {
   addOrderForm: orderForm = new orderForm();
+  orderFormValidate: OrderValidator = new OrderValidator();
+  validateResult: any = null;
 
   @ViewChild("orderForm")
   orderForm!: NgForm;
@@ -19,9 +23,10 @@ export class AddOrderComponent implements OnInit {
 
   ngOnInit(): void {  }
 
-  AddOrder(isValid: any) {
+  AddOrder() {
     this.isSubmitted = true;
-    if (isValid) {
+    this.validateResult = this.orderFormValidate.validate(this.addOrderForm);
+    if (Object.keys(this.validateResult).length == 0) {
       this.httpProvider.saveOrder(this.addOrderForm).subscribe(async data => {
         if (data != null && data.status == 200) {
           this.toastr.success("Added successfully.");
@@ -53,4 +58,25 @@ export class orderForm {
       }
     }
   ];
+}
+
+class OrderValidator extends Validator<orderForm> {
+  constructor() {
+    super();
+
+    this.ruleFor('shippingAdress')
+      .notEmpty()
+      .withMessage('Please enter Shipping Address')
+      .maxLength(256)
+      .withMessage('Shipping Address length was exceed.');
+
+    this.ruleFor('orderItemsDtoModel')
+    .notNull()
+    .must(items => items[0].productId >= 1 && items[0].productId < 99999)
+    .withMessage('Please enter right product id.')
+    .must(items => items[0].price.amount > 0 && items[0].price.amount < 100)
+    .withMessage('Please enter right amount.')
+    .must(items => items[0].price.unit >= 0 && items[0].price.unit < 4)
+    .withMessage('Please enter right unit.');
+  }
 }
--- EShopping.Frontend/src/app/edit-order/edit-order.component.ts
+++ EShopping.Frontend/src/app/edit-order/edit-order.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HttpProviderService } from '../Service/http-provider.service';
+import { Validator } from 'fluentvalidation-ts';
 
 @Component({
   selector: 'app-edit-order',
@@ -11,6 +12,8 @@ import { HttpProviderService } from '../Service/http-provider.service';
 })
 export class EditOrderComponent implements OnInit {
   editOrderForm: orderForm = new orderForm();
+  orderUpdateFormValidate: OrderUpdateValidator = new OrderUpdateValidator();
+  validateResult: any = null;
 
   @ViewChild("orderForm")
   orderForm!: NgForm;
@@ -44,9 +47,10 @@ export class EditOrderComponent implements OnInit {
       (error: any) => { });
   }
 
-  EditOrder(isValid: any) {
+  EditOrder() {
     this.isSubmitted = true;
-    if (isValid) {
+    this.validateResult = this.orderUpdateFormValidate.validate(this.editOrderForm);
+    if (Object.keys(this.validateResult).length == 0) {
       this.httpProvider.updateOrder(this.editOrderForm).subscribe(async data => {
         if (data != null && data.status == 200) {
           this.toastr.success("Updated successfully.");
@@ -82,4 +86,25 @@ export class orderForm {
       }
     }
   ];
+}
+
+class OrderUpdateValidator extends Validator<orderForm> {
+  constructor() {
+    super();
+
+    this.ruleFor('shippingAdress')
+      .notEmpty()
+      .withMessage('Please enter Shipping Address')
+      .maxLength(256)
+      .withMessage('Shipping Address length was exceed.');
+
+    this.ruleFor('orderItemsDtoModel')
+    .notNull()
+    .must(items => items[0].productId >= 1 && items[0].productId < 99999)
+    .withMessage('Please enter right product id.')
+    .must(items => items[0].price.amount > 0 && items[0].price.amount < 100)
+    .withMessage('Please enter right amount.')
+    .must(items => items[0].price.unit >= 0 && items[0].price.unit < 4)
+    .withMessage('Please enter right unit.');
+  }
 }
